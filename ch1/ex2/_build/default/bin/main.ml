29d49d566b1c90feb572f03e5221d079f068bcf7
Lewis Sneddon <lewis@lewiscodes.io>
2024-01-22 08:38:36 +0000

    demo project to play with

--- ch1/ex2/_build/default/bin/main.ml
+++ ch1/ex2/_build/default/bin/main.ml
@@ -1,4 +1,4 @@
 let x = -10
-let a = if x < 0 then x * -1 else x
+let a = if x < 0 then -x else x
 let result = Printf.sprintf "%d" a
 let () = print_endline result
---
+++ demo/bin/main.ml
@@ -0,0 +1,3 @@
+let () =
+        let s = if 42 mod 2 = 0 then "even" else "odd" in
+                print_endline ("The number is " ^ s)
